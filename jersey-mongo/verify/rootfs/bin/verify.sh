1962ff70788cb8b3978ee5b526621822d6b4fafb
sjkyspa <sjkyspa@gmail.com>
2016-09-23 11:07:19 +0800

    launch lambda in the verify image in jersey mongo stack

--- jersey-mongo/verify/rootfs/bin/verify.sh
+++ jersey-mongo/verify/rootfs/bin/verify.sh
@@ -11,22 +11,33 @@ set -eo pipefail
 
 on_exit() {
     last_status=$?
+    trap '' HUP INT TERM QUIT ABRT EXIT
+    local exit_status=0
     if [ "$last_status" != "0" ]; then
         if [ -f "process.log" ]; then
           cat process.log
         fi
-
-        exit 1;
-    else
-        exit 0;
+        exit_status=1
     fi
+
+    if [ "$LAMBDA_URI" != "" ]; then
+        lambda deprovision --lambda-uri "$LAMBDA_URI"
+    fi
+
+    trap - HUP INT TERM QUIT ABRT EXIT
+    exit ${exit_status}
 }
 
 trap on_exit HUP INT TERM QUIT ABRT EXIT
 
+echo "Launch lambda env..."
+LAMBDA_URI=$(lambda provision --build-uri "$BUILD_URI")
+echo "Launch lambda success"
+
 cd $CODEBASE
-ENDPOINT_HOST=$(echo $LAMBDA|jq --raw-output '.services.web.endpoint.internal.host')
-ENDPOINT_PORT=$(echo $LAMBDA|jq --raw-output '.services.web.endpoint.internal.port')
+LAMBDA_INFO=$(lambda info --lambda-uri $LAMBDA_URI)
+ENDPOINT_HOST=$(echo $LAMBDA_INFO|jq --raw-output '.services.web.endpoint.internal.host')
+ENDPOINT_PORT=$(echo $LAMBDA_INFO|jq --raw-output '.services.web.endpoint.internal.port')
 echo
 echo "Building verify jar..."
 ENDPOINT="http://$ENDPOINT_HOST:$ENDPOINT_PORT" GRADLE_USER_HOME="$CACHE_DIR" gradle itest 
